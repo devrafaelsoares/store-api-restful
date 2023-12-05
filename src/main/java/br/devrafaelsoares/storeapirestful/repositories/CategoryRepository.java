ddebf76158cb1b5f5dcb3b690c0cfa35260bdc6f
devrafaelsoares <rafael.soares.developer@gmail.com>
2023-12-05 10:34:53 -0300

    feature: implementing category repository

---
+++ src/main/java/br/devrafaelsoares/storeapirestful/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package br.devrafaelsoares.storeapirestful.repositories;
+
+import br.devrafaelsoares.storeapirestful.domain.category.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    Optional<Category> findByName(
+            String name
+    );
+}
