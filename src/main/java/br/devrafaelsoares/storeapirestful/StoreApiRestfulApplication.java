3292dca591dbf15e007d545a496418669d28599c
devrafaelsoares <rafael.soares.developer@gmail.com>
2023-12-04 21:48:46 -0300

    refactor: start project

---
+++ src/main/java/br/devrafaelsoares/storeapirestful/StoreApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package br.devrafaelsoares.storeapirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApiRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApiRestfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/devrafaelsoares/storeapirestful/StoreApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.devrafaelsoares.storeapirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
