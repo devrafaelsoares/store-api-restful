d11d63bf07450b0c2b097e3b47fac55f0626b195
devrafaelsoares <rafael.soares.developer@gmail.com>
2023-12-05 10:34:43 -0300

    feature: implementing category entity

---
+++ src/main/java/br/devrafaelsoares/storeapirestful/domain/category/Category.java
@@ -0,0 +1,34 @@
+package br.devrafaelsoares.storeapirestful.domain.category;
+
+import br.devrafaelsoares.storeapirestful.domain.product.Product;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<Product> products = new HashSet<>();
+
+}
