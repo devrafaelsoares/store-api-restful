37d5d40a7cf9a8fa0b609b7ad39ed3c4e9de8638
devrafaelsoares <rafael.soares.developer@gmail.com>
2023-12-11 23:47:47 -0300

    feature: Implementing validation of category DTOs

--- src/main/java/br/devrafaelsoares/storeapirestful/domain/category/dto/CategoryUpdateRequest.java
+++ src/main/java/br/devrafaelsoares/storeapirestful/domain/category/dto/CategoryUpdateRequest.java
@@ -1,9 +1,11 @@
 package br.devrafaelsoares.storeapirestful.domain.category.dto;
 
+import br.devrafaelsoares.storeapirestful.domain.validations.constraints.NotBlankOrNull;
 import lombok.Getter;
 
 @Getter
 public record CategoryUpdateRequest (
+        @NotBlankOrNull(message = "Campo obrigatório")
         String name
 ) implements CategoryUpdate
 {}
