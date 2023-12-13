b25717502eb48b836309b224d26578b451aa2e73
devrafaelsoares <rafael.soares.developer@gmail.com>
2023-12-12 22:42:34 -0300

    feature: Implementing the JWT filter

--- src/main/java/br/devrafaelsoares/storeapirestful/infra/security/SpringSecurityConfig.java
+++ src/main/java/br/devrafaelsoares/storeapirestful/infra/security/SpringSecurityConfig.java
@@ -1,19 +1,29 @@
 package br.devrafaelsoares.storeapirestful.infra.security;
 
 import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import static br.devrafaelsoares.storeapirestful.domain.user.Role.*;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -49,7 +59,10 @@ public class SpringSecurityConfig {
 
                         .anyRequest()
                             .authenticated()
+
                 )
+                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                    .authenticationProvider(authenticationProvider)
                 .build();
     }
 }
