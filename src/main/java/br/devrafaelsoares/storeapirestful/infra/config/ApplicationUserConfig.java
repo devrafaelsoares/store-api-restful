cfbc4a882ab52b7c87a8fb4e3581e71f58abf62e
devrafaelsoares <rafaelsoares.developer@gmail.com>
2024-01-03 09:39:28 -0300

    feat: implementing application admin user

---
+++ src/main/java/br/devrafaelsoares/storeapirestful/infra/config/ApplicationUserConfig.java
@@ -0,0 +1,41 @@
+package br.devrafaelsoares.storeapirestful.infra.config;
+
+import br.devrafaelsoares.storeapirestful.domain.user.User;
+import br.devrafaelsoares.storeapirestful.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import static br.devrafaelsoares.storeapirestful.domain.user.Role.ADMIN;
+
+@Component
+public class ApplicationUserConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Value("${spring.application.security.username}")
+    private String username;
+
+    @Value("${spring.application.security.password}")
+    private String password;
+
+    @Override
+    public void run(String... args) {
+
+        if (userRepository.findByUsername(username).isEmpty()) {
+
+            User userAdmin = User
+                    .builder()
+                    .username(username)
+                    .password(new BCryptPasswordEncoder().encode(password))
+                    .role(ADMIN)
+                    .build();
+
+            userRepository.save(userAdmin);
+        }
+
+    }
+}
