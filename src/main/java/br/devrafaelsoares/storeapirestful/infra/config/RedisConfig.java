9c642d784b69c53d8cd61f18af89ca79331841bc
devrafaelsoares <rafaelsoares.developer@gmail.com>
2024-01-03 09:39:46 -0300

    feat: implementing redis config

---
+++ src/main/java/br/devrafaelsoares/storeapirestful/infra/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package br.devrafaelsoares.storeapirestful.infra.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Value("${spring.redis.expiration}")
+    private int cacheRedisExpiration;
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(cacheRedisExpiration))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+}
