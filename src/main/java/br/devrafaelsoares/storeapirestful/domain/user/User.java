da22bfdcc4d76269b09df7247106f760e134425f
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-01-18 07:52:14 -0300

    feat: implementing the email and name fields in the user entity

--- src/main/java/br/devrafaelsoares/storeapirestful/domain/user/User.java
+++ src/main/java/br/devrafaelsoares/storeapirestful/domain/user/User.java
@@ -1,12 +1,17 @@
 package br.devrafaelsoares.storeapirestful.domain.user;
 
+import br.devrafaelsoares.storeapirestful.domain.cart.Cart;
+import br.devrafaelsoares.storeapirestful.domain.order.Order;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
@@ -26,8 +31,15 @@ public class User implements Serializable, UserDetails {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
+    private String name;
+
     @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(unique = true, nullable = false)
+    @Email(message = "E-mail inválido")
+    private String email;
 
     @Column(nullable = false, length = 256)
     private String password;
@@ -35,6 +47,17 @@ public class User implements Serializable, UserDetails {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToMany(
+        mappedBy = "user", fetch = FetchType.EAGER
+    )
+    @JsonIgnore
+    private List<Order> orders = new ArrayList<>();
+
+    @OneToOne(
+            mappedBy = "user", cascade = { CascadeType.PERSIST, CascadeType.MERGE }, orphanRemoval = true, fetch = FetchType.EAGER
+    )
+    private Cart cart;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
