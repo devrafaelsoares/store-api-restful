09673ca6a3bea4a129b0b031169b2ec7c3cf51ac
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-01-18 07:54:06 -0300

    feat: configuring the creation of the default user of the user entity for the new fields

--- src/main/java/br/devrafaelsoares/storeapirestful/infra/config/ApplicationUserConfig.java
+++ src/main/java/br/devrafaelsoares/storeapirestful/infra/config/ApplicationUserConfig.java
@@ -1,6 +1,8 @@
 package br.devrafaelsoares.storeapirestful.infra.config;
 
+import br.devrafaelsoares.storeapirestful.domain.cart.Cart;
 import br.devrafaelsoares.storeapirestful.domain.user.User;
+import br.devrafaelsoares.storeapirestful.repositories.CartRepository;
 import br.devrafaelsoares.storeapirestful.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,9 +18,15 @@ public class ApplicationUserConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Value("${spring.application.security.name}")
+    private String name;
     @Value("${spring.application.security.username}")
     private String username;
-
+    @Value("${spring.application.security.email}")
+    private String email;
     @Value("${spring.application.security.password}")
     private String password;
 
@@ -29,12 +37,21 @@ public class ApplicationUserConfig implements CommandLineRunner {
 
             User userAdmin = User
                     .builder()
+                        .name(name)
+                        .email(email)
                         .username(username)
                         .password(new BCryptPasswordEncoder().encode(password))
                         .role(ADMIN)
                     .build();
 
             userRepository.save(userAdmin);
+
+            Cart cart = Cart
+                    .builder()
+                        .user(userAdmin)
+                    .build();
+
+            cartRepository.save(cart);
         }
 
     }
