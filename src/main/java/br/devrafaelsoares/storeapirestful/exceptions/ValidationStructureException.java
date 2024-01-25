26bf737a44cba90e9ccffcf88050a055d691b8dd
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-01-25 16:10:24 -0300

    style: moving the creation of exception response structures to a separate module

---
+++ src/main/java/br/devrafaelsoares/storeapirestful/exceptions/ValidationStructureException.java
@@ -0,0 +1,25 @@
+package br.devrafaelsoares.storeapirestful.exceptions;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationStructureException {
+
+    private ZonedDateTime moment;
+
+    private Integer status;
+
+    private List<FieldValidationStructureException> errors;
+
+    private String path;
+}
